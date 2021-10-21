31f3aabbd4b0a3fc16c0d79b6077e911871704cf
rogerkorp <rmk347@drexel.edu>
2021-10-21 18:44:17 -0400

    :fire: Rewrote Some Code
    Should be able to get the banner working soon, but I had to convert the files into PHP Friendly ones.

---
+++ alpha.php
@@ -0,0 +1,34 @@
+<!-- IDM 232: Alpha Build -->
+<!-- Written by Roger Korpics -->
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Roger's Recipe Book</title>
+        <link rel="stylesheet" href="stylesheet.css">
+    </head>
+    <body id=home>
+        
+        <main id="homepage-main">
+            <h2>Roger K's</h2>
+            <h1>Recipe Book</h1>
+            
+            <picture>
+                <source media="(min-width: 768px)" srcset="https://via.placeholder.com/600x450"/>
+                <img src="https://via.placeholder.com/400x300">
+            </picture>  
+        </main>
+        
+        <nav id="homepage-nav">
+            <a>Search</a>
+            <a>Browser</a>
+        </nav>
+
+        <?php
+        include 'footer.php'
+        ?>
+
+    </body>
+</html>
